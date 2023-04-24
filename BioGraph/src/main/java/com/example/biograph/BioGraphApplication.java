1b32ab260fd813da5563caf19f07d5d38199493c
laisRamos1234 <Lais.Barbosa@Amway.com>
2023-04-24 19:35:09 -0300

    Inicio da criação dos módulos

---
+++ BioGraph/src/main/java/com/example/biograph/BioGraphApplication.java
@@ -0,0 +1,13 @@
+package com.example.biograph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioGraphApplication.class, args);
+	}
+
+}
---
+++ BioGraph/src/test/java/com/example/biograph/BioGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biograph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
