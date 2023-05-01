0f7ac279b95232b9db07064610ef6791f1bb203b
Taynara <taynaracris11@hotmail.com>
2023-05-01 16:06:29 -0300

    criando base do front

--- BioGraph/frontend/bioGraphUi/src/app/app-routing.module.ts
+++ BioGraph/frontend/bioGraphUi/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'grafo' },
+  {
+    path: 'grafo',
+    loadChildren: () => import('./grafo/grafo.module').then(m => m.GrafoModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
